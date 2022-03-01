c80b4a6394729d09b1afff3d5deed0f74a9dfbcd
J.Son <91623332+jrson11@users.noreply.github.com>
2022-03-01 14:05:39 -0600

    Update Civil_Geo-Cc_Cr_Cs.py

--- streamlit/Civil_Geo-Cc_Cr_Cs.py
+++ streamlit/Civil_Geo-Cc_Cr_Cs.py
@@ -54,7 +54,7 @@ plt.plot(np.log(x),y, '.-')
 plt.scatter(np.log(x[Px]),y[Px], c='k', marker='x')
 plt.scatter(np.log(x[Pc]),y[Pc], c='r', marker='x')
 plt.scatter(np.log(x[Pr]),y[Pr], c='m', marker='x')
-plt.plot([np.log(x[Pc]),np.log(xmin)],[y[Pc],0.7], 'r--')
+plt.plot([np.log(x[Pc]),np.log(xmin)],[y[Pc],find_y2(np.log(x[Pc]),np.log(y[Pc])),Cc], 'r--')
 #
 plt.xlabel('log(x)')
 plt.ylabel('y')
