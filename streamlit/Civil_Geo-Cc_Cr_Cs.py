d6ab56bdf4593c89cda64bf0ba604e6a6ee300b4
J.Son <91623332+jrson11@users.noreply.github.com>
2022-03-01 13:30:44 -0600

    Update Civil_Geo-Cc_Cr_Cs.py

--- streamlit/Civil_Geo-Cc_Cr_Cs.py
+++ streamlit/Civil_Geo-Cc_Cr_Cs.py
@@ -27,7 +27,7 @@ Cs = st.sidebar.slider('Cs: Swelling Index', 0.0, 0.5, 0.0)
 fig = plt.figure(figsize = (5,5), dpi=100)
 plt.rcParams['font.size'] = '8'
 plt.plot(x,y, '.-')
-plt.plot([0,0],[0.6,0.6], 'r--')
+plt.plot([0,100],[0.6,0.6], 'r--')
 #
 plt.xlabel('x')
 plt.ylabel('y')
