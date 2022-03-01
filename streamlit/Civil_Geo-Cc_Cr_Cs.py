aa1192290740047d5b9e26f9a9df877dd012cb6d
J.Son <91623332+jrson11@users.noreply.github.com>
2022-03-01 14:51:29 -0600

    Update Civil_Geo-Cc_Cr_Cs.py

--- streamlit/Civil_Geo-Cc_Cr_Cs.py
+++ streamlit/Civil_Geo-Cc_Cr_Cs.py
@@ -74,7 +74,6 @@ plt.plot([np.log(x[Px]),np.log(xmax)],[y[Px],y[Px]], 'k-')
 plt.plot([np.log(x[Pc]),np.log(xmin)],[y[Pc],find_y2(np.log(x[Pc]),y[Pc],Cc,np.log(xmin))], 'r--')
 plt.plot([np.log(x[Pr]),np.log(xmax)],[y[Pr],find_y2(np.log(x[Pr]),y[Pr],Cr,np.log(xmax))], 'm--')
 plt.plot([np.log(x[Pr]),np.log(xmax)],[y[Pr],find_y2(np.log(x[Pr]),y[Pr],Cs,np.log(xmax))], 'y--')
-plt.text(np.log(xmax)-0.1,find_y2(np.log(x[Pc]),y[Pc],Cc,np.log(xmin))], 'Cc')
 
 ## Pc
 plt.plot([np.log(Pc_kPa),np.log(Pc_kPa)],[ymin-0.03,y[Px]], 'k-')
