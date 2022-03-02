01cf0fb361d2e7a5ec1ff99e0f5128694caa2a88
J.Son <91623332+jrson11@users.noreply.github.com>
2022-03-02 08:54:38 -0600

    Update Civil_Geo-Cc_Cr_Cs.py

--- streamlit/Civil_Geo-Cc_Cr_Cs.py
+++ streamlit/Civil_Geo-Cc_Cr_Cs.py
@@ -27,8 +27,8 @@ if password == st.secrets['db_password']:
   #
   st.dataframe(df)
   cols = df.columns
-  #samp_list = np.unique(df.cols[0])
-  st.text(cols)
+  samp_list = np.unique(df.cols[0])
+  st.text(samp_list)
   #samp_ID = st.sidebar.selectbox('Select Sample ID', np.arange(nx), 102) 
 else:
   st.markdown('## Please join KUPEA')
