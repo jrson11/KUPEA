acd69cd9dab109984b9a342598ccdad8d4ed290b
J.Son <91623332+jrson11@users.noreply.github.com>
2022-03-04 13:10:58 -0600

    Update Excel-Combine_multiple_CSV.py

--- streamlit/Excel-Combine_multiple_CSV.py
+++ streamlit/Excel-Combine_multiple_CSV.py
@@ -22,8 +22,41 @@ if password == st.secrets['db_password']:
   st.sidebar.markdown('#### Input files')
   st.sidebar.text('Number of uploaded files: '+str(n_input))
   
-
 else:
   st.markdown('## Please join KUPEA')
   st.markdown('#### Only members can upload his own data to combine multiple CSV files')
+
   
+# Processing
+n_header = st.sidebar.selectbox('Number of header lines to skip',[0,1,2,3,4,5,6,7,8,9,10])
+
+df_XLSX = pd.DataFrame()
+for input_file in input_list:
+    st.text(input_file.name)
+    if t_data == 'xlsx':
+        df_xlsx = pd.read_excel(input_file, header=n_header)
+    elif t_data == 'xlsx':
+        df_xlsx = pd.read_csv(input_file, header=n_header)
+    df_xlsx.insert(0,'File',input_file.name)
+    df_XLSX = pd.concat([df_XLSX,df_xlsx.loc[1:,:]])    
+
+st.markdown('#### Output result')
+st.dataframe(df_XLSX)
+
+
+# Export
+def convert_df(df):
+     # IMPORTANT: Cache the conversion to prevent computation on every rerun
+     return df.to_csv(index=False).encode('utf-8')
+
+
+csv_result = convert_df(df_XLSX)
+
+st.sidebar.markdown('## Output result')
+
+st.sidebar.download_button(
+     label="Download result table",
+     data=csv_result,
+     file_name='all_data.csv',
+     mime='text/csv',
+ )
