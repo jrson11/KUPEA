117fd3c32862a645be67a782940136e4ccac3290
J.Son <91623332+jrson11@users.noreply.github.com>
2022-03-04 13:06:37 -0600

    Update Excel-Combine_multiple_CSV.py

--- streamlit/Excel-Combine_multiple_CSV.py
+++ streamlit/Excel-Combine_multiple_CSV.py
@@ -1,1 +1,25 @@
+# Purspose: To combine multiple CSV files, which have the same format
+# Name: Excel-Combine_multiple_CSV.py
+# Author: glocivilngneer at gmail dot com
+# All right reserved
 
+import numpy as np
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+
+# Initialization ----------------------------------------
+st.markdown('## Combine multiple CSV files')
+password = st.sidebar.text_input('Password to Continue', 'password')
+
+# Membership --------------------------------------------
+if password == st.secrets['db_password']:
+  st.markdown('## Welcome to KUPEA')
+  st.markdown('#### You can upload your CSV file with 3 columns [Sample_ID, X, Y]')
+  #
+  csv = st.file_uploader('Choose input file',type=['csv'], accept_multiple_files=False)
+
+else:
+  st.markdown('## Please join KUPEA')
+  st.markdown('#### Only members can upload his own data to analyze engineering parameters')
+  
