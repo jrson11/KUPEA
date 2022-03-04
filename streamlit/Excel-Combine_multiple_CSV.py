2dd4051a67ed985940764cb41f24978238f06b71
J.Son <91623332+jrson11@users.noreply.github.com>
2022-03-04 13:43:34 -0600

    Update Excel-Combine_multiple_CSV.py

--- streamlit/Excel-Combine_multiple_CSV.py
+++ streamlit/Excel-Combine_multiple_CSV.py
@@ -4,13 +4,27 @@ import streamlit as st
 # Initialization ----------------------------------------
 st.markdown('## To Combine Multiple Excel files')
 password = st.sidebar.text_input('Password to Continue', 'password')
+#
+st.sidebar.markdown('## Input')
+t_data = st.sidebar.selectbox('Type of input data file',['xlsx','xls','csv'])
+#
+if t_data == 'xlsx':
+    input_list = st.sidebar.file_uploader('Choose input files',type=['xlsx'], accept_multiple_files=True)
+elif t_data == 'xls':
+    input_list = st.sidebar.file_uploader('Choose input files',type=['xls'], accept_multiple_files=True)
+elif t_data == 'csv':
+    input_list = st.sidebar.file_uploader('Choose input files',type=['csv'], accept_multiple_files=True)
+  
+n_input = len(input_list)
+st.markdown('#### Input files')
+st.text('Number of uploaded files: '+str(n_input))
+
+
+# Importing ----------------------------------------
 
 
 
-
-
-
-
+# Process ----------------------------------------
 
 
 '''
