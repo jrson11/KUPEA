ac859464b58b9b9a16f55fc499a191bc75ef049c
J.Son <91623332+jrson11@users.noreply.github.com>
2022-03-04 13:48:57 -0600

    Update Excel-Combine_multiple_CSV.py

--- streamlit/Excel-Combine_multiple_CSV.py
+++ streamlit/Excel-Combine_multiple_CSV.py
@@ -25,7 +25,8 @@ st.sidebar.text('Number of uploaded files: '+str(n_input))
 # Process ----------------------------------------
 n_header = st.sidebar.selectbox('Number of header lines to skip',[0,1,2,3,4,5,6,7,8,9,10],0)
 
-st.text(input_list[0].name)
+input_file = input_list[0].name
+st.text(input_file)
 
 '''
 
