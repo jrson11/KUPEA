a1c910c427c0f93d62b912fb4bb5be7115df83f2
J.Son <91623332+jrson11@users.noreply.github.com>
2022-03-04 13:36:44 -0600

    Update Excel-Combine_multiple_CSV.py

--- streamlit/Excel-Combine_multiple_CSV.py
+++ streamlit/Excel-Combine_multiple_CSV.py
@@ -35,7 +35,7 @@ for input_file in input_list:
     st.text(input_file.name)
     if t_data == 'xlsx':
         st.text('xlsx')
-        df_xlsx = pd.read_excel(input_file.name)
+        df_xlsx = pd.read_excel(input_file)
         st.dataframe(df_xlsx)
     elif t_data == 'xls':
         st.text('xls')
