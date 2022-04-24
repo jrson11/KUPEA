398fc89ababf16fb9305633fdfa534934ffac862
J.Son <91623332+jrson11@users.noreply.github.com>
2022-04-24 17:18:18 -0500

    Update Civil_Geo-CPT_processing.py

--- streamlit/Civil_Geo-CPT_processing.py
+++ streamlit/Civil_Geo-CPT_processing.py
@@ -1,1 +1,13 @@
+# Purspose: To derive engineering parameters from CPT in-situ data
+# Name: Civil_Geo-CPT_processing.py
+# Author: glocivilngneer at gmail dot com
+# All right reserved
+
+import numpy as np
 import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+
+# Initialization ----------------------------------------
+st.markdown('## Deriving ENG parmeters from CPT')
+password = st.sidebar.text_input('Password to Continue', 'password')
